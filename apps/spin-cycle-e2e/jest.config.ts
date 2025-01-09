437640dbb7635480733e826b7c1685f512c23021
Lawrence Davis <lawrence@lrdiv.co>
2025-01-09 15:05:37 -0600

    Initial commit
    To connect your workspace to Nx Cloud, push your repository
    to your git hosting provider and go to the following URL:
    
    https://cloud.nx.app/connect/P2LBLwKwxv

---
+++ apps/spin-cycle-e2e/jest.config.ts
@@ -0,0 +1,18 @@
+export default {
+  displayName: 'spin-cycle-e2e',
+  preset: '../../jest.preset.js',
+  globalSetup: '<rootDir>/src/support/global-setup.ts',
+  globalTeardown: '<rootDir>/src/support/global-teardown.ts',
+  setupFiles: ['<rootDir>/src/support/test-setup.ts'],
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+      },
+    ],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/spin-cycle-e2e',
+};
